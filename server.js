2d977e782c146c3c65a0e0da44361afd50e88dd9
Oleksii Verbytskyi <alverba88@gmail.com>
2023-02-12 12:21:47 +0200

    mod server

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+const mongoose = require('mongoose');
+require("dotenv").config();
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const { DB_HOST, PORT= 3000 } = process.env;
+
+mongoose.connect(DB_HOST, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+    .then(() => {
+      app.listen(PORT);
+      console.log("Database connect successful");
+    })
+    .catch(error => {
+      console.log(error);
+      process.exit(1);
+    })
